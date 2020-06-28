65a21af6860302da73064b2626d830bfabbe7ecf
priyankapatil <ppatil@gigsky.com>
2020-06-28 20:26:15 +0530

    Removed unused code

--- src/app/pages/carousel/carousel.component.ts
+++ src/app/pages/carousel/carousel.component.ts
@@ -11,22 +11,6 @@ export class CarouselComponent implements OnInit {
   errorMessage: String;
   usersAnswerList: any[];
 
-  items = [
-    {id: 1, name: 'Python'},
-    {id: 2, name: 'Node Js'},
-    {id: 3, name: 'Java'},
-    {id: 4, name: 'PHP', disabled: true},
-    {id: 5, name: 'Django'},
-    {id: 6, name: 'Angular'},
-    {id: 7, name: 'Vue'},
-    {id: 8, name: 'ReactJs'},
-  ];
-  selected = [
-    {id: 2, name: 'Node Js'},
-    {id: 8, name: 'ReactJs'}
-  ];
-  name = 'User';
-
   constructor(config: NgbCarouselConfig, private service: BackendServices) {
     config.interval = 20000;
     config.wrap = true;
